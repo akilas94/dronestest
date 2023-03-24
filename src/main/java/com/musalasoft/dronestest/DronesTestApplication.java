4e48df9e166e724e240e9fc109547e8e0b202099
akila <akilasilva94@gmail.com>
2023-03-24 23:34:35 +0800

    Initial commit

---
+++ src/main/java/com/musalasoft/dronestest/DronesTestApplication.java
@@ -0,0 +1,12 @@
+package com.musalasoft.dronestest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DronesTestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/musalasoft/dronestest/DronesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musalasoft.dronestest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
